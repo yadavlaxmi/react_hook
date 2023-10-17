04f2b3d2a87dfc985a8579ca2716cf95e049c5e1
laxmi <laxmiyadav21@navgurukul.org>
2023-10-17 17:57:10 +0530

    clear data of array

--- src/Component/UseState_Array.js
+++ src/Component/UseState_Array.js
@@ -1,31 +1,36 @@
-import React from "react"
+import React ,{useState}from "react"
 import { Button } from "@mui/material"
 const UseState_Array=()=>{
-    const bioData=[
+    
+
+    var bioData=[
         {
         id:0,myName:"laxmi",age:17
     },
     {
         id:1,myName:"mansa",age:22
-    },{
+    },
+    {
         id:2,myName:"guddi",age:21
 
     }
         
     ]
-    console.log(bioData)
+    const[myArray,setmyArray]=useState(bioData);
+    const clearArray=()=>{
+        setmyArray([]);
+    }
     return(
-        <div>
+        <>
             {
-                bioData.map((currentElement)=>{
+                myArray.map((currentElement)=>{
                     return(
-                            <Button variant="outlined">Name:{currentElement.myName} and Age:{currentElement.age}</Button> 
-
-                    )
-
-                })
+                            <h1>Name:{currentElement.myName} and Age:{currentElement.age}</h1>
+                        )})
             }
-        </div>
+            <Button variant="contained" color="secondary" onClick={clearArray}>clear</Button>
+
+        </>
     )
 }
 export default UseState_Array
