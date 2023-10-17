d2398d4cafa57644d04c60a2d44a5e28315ef5c8
laxmi <laxmiyadav21@navgurukul.org>
2023-10-17 22:41:21 +0530

    short circuit with and operator

--- src/Component/Short_circuit.js
+++ src/Component/Short_circuit.js
@@ -1,12 +1,12 @@
 import React,{useState} from "react"
 const Short_circuit=()=>{
-    const[demo,setDemo]=useState("")
+    const[demo,setDemo]=useState("laxmi")
 
     return(
         <>
             <h1> hii short cuircuit page</h1>
             <h2> {demo || "guddi"} </h2>
-            <h2>govind</h2>
+            <h2>{demo && "govind"}</h2>
 
         </>
     )
