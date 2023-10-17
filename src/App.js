065f7479f3c9b341e6252424929be303d0d96488
laxmi <laxmiyadav21@navgurukul.org>
2023-10-17 13:01:49 +0530

    react_hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 function App() {
+  var val="Meraki app"
+  const changeName=()=>{
+        val="laxmi"
+        console.log(val)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='App'>
+      <h1>{val}</h1>
+      <button className='btn' onClick={changeName}>click me</button>
     </div>
   );
 }
