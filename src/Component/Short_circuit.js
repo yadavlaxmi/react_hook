aee5258d3215985db62bfa0de59c22124ffeed34
laxmi <laxmiyadav21@navgurukul.org>
2023-10-17 22:44:42 +0530

    short circuit evaluation complete

--- src/Component/Short_circuit.js
+++ src/Component/Short_circuit.js
@@ -1,11 +1,14 @@
 import React,{useState} from "react"
 const Short_circuit=()=>{
-    const[demo,setDemo]=useState("laxmi")
+    const[demo,setDemo]=useState("")
 
     return(
         <>
             <h1> hii short cuircuit page</h1>
-            <h2> {demo || "guddi"} </h2>
+            <h2> {demo || <>
+            <h1> Hii</h1>
+            <h2>Everyone</h2>
+            </>} </h2>
             <h2>{demo && "govind"}</h2>
 
         </>
