1dfdf87fd2e3546cc8c8e5653a14405bec63d53f
laxmi <laxmiyadav21@navgurukul.org>
2023-10-18 15:24:48 +0530

    onChange event

--- src/Component/Forms.js
+++ src/Component/Forms.js
@@ -1,19 +1,23 @@
+import React,{useState} from "react"
 import { Button } from "@mui/material"
 const Forms=()=>{
+    const[name , setName]=useState("");
+    const[email , setEmail]=useState("");
+    const[password,setPassword]=useState("")
     return(
     <>
         <form action="">
             <div>
                 <label htmlFor="Name">Name</label>
-                <input type="text" name="Name" id="Name"/>
+                <input type="text" name="Name" id="Name" autoComplete="off" value={name} onChange={(e)=> setName(e.target.value)}/>
             </div><br></br>
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" autoComplete="off"/>
+                <input type="text" name="email" id="email" autoComplete="off" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div><br></br>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="text" name="password" id="password"/>
+                <input type="password" name="password" id="password" autoComplete="off" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div><br></br>
             <Button type ="submit" variant="contained"> logIn </Button>
 
