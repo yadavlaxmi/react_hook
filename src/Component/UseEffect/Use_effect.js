9306e3eb6c54bcd55191a7de5516b4ffddc266e5
laxmi <laxmiyadav21@navgurukul.org>
2023-10-18 19:52:27 +0530

    useeffect in title

--- src/Component/UseEffect/Use_effect.js
+++ src/Component/UseEffect/Use_effect.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Button from "@mui/material/Button"; 
+import Button from "@mui/material/Button";
 
 const UseEffectExample = () => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    document.title="chats(${count})"
-  }); 
+    document.title = `Chats (${count})`; 
+  }, [count]); 
 
   console.log("hello outside");
 
@@ -16,7 +16,7 @@ const UseEffectExample = () => {
       <Button
         variant="contained"
         color="primary"
-        onClick={() => setCount(count + 1)} 
+        onClick={() => setCount(count + 1)}
       >
         Click
       </Button>
