5dfe62276defe4dbd5725fbf0d46f0679811f102
laxmi <laxmiyadav21@navgurukul.org>
2023-10-19 21:30:05 +0530

    resize the size of dispaly screen

--- src/Component/UseEffect/Clean_up.js
+++ src/Component/UseEffect/Clean_up.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react"
 const Clean_up=()=>{
     const[widthCount ,setWidthCount]=useState(window.screen.width);
     const actualWidth=()=>{
-        console.log (window.innerWidth)
+        setWidthCount(window.innerWidth)
     }
     useEffect(()=>{
         window.addEventListener("resize", actualWidth);
