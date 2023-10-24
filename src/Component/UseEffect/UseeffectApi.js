b36aafdaf59fd334fd11a4b5da86c3fbdda576e4
laxmi <laxmiyadav21@navgurukul.org>
2023-10-24 20:19:23 +0530

    fetching Api for rendering one

--- src/Component/UseEffect/UseeffectApi.js
+++ src/Component/UseEffect/UseeffectApi.js
@@ -1,12 +1,14 @@
-import React,{useEffect} from "react";
+import React,{useState,useEffect} from "react";
 const UseeffectApi=()=>{
+    const[users,setUsers]=useState([]);
     const getUsers=async()=>{
         const response= await fetch("https://jsonplaceholder.typicode.com/posts");
-        console.log(response)
+        setUsers(await response.json())
+        // console.log(data)
     }
     useEffect(()=>{
         getUsers();
-    })
+    },[])
     return(
         <>
             <h1 className='h1'>Useeffect Api</h1>
