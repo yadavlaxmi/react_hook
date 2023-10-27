0c1a963fff24876cd46f031e75b7556a3a636c29
laxmi <laxmiyadav21@navgurukul.org>
2023-10-27 11:21:30 +0530

    Useref hook

--- src/Api/Useref/Useref1.js
+++ src/Api/Useref/Useref1.js
@@ -1,17 +1,21 @@
 import React ,{useState,useEffect,useRef} from "react";
 const Useref1=()=>{
     const[myData,setMyData]=useState("")
-    const[count,setCount]=useState(0);
+    // const[count,setCount]=useState(0);
+    const count=useRef(0)
+    console.log(count)
     useEffect(()=>{
-        setCount(count+1)
-    })
+        count.current=count.current+1
+        // setCount(count+1)
+    })  
     return(
         <>
+        
         <input
         type="text"
         value={myData}
         onChange={(e)=>setMyData(e.target.value)}/>
-        <p> This number of times render : {count}</p>
+        <p> This number of times render : {count.current}</p>
         </>
     )
 }
